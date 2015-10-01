38e7bc3881a6076bae28baaf360afc755371c45f
bphermansson <patrik@paheco.nu>
2015-10-01 07:53:03 +0200

    Working version

--- app/src/main/java/nu/paheco/patrik/emondatalight/MainActivity.java
+++ app/src/main/java/nu/paheco/patrik/emondatalight/MainActivity.java
@@ -1,17 +1,100 @@
 package nu.paheco.patrik.emondatalight;
 
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class MainActivity extends ListActivity {
+
+    // URL to get Emoncms JSON data
+    private static String url = "http://emoncms.org/feed/list.json?apikey=8133697b1b562f52689bd680b330cb4d";
+
+    // JSON Node names
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_TIME = "time";
+    private static final String TAG_VALUE = "value";
+
+    // Hashmap for ListView
+    ArrayList<HashMap<String, String>> dataList;
+    String datatime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView header = (TextView) findViewById(R.id.header);
+
+        // Init list
+        dataList = new ArrayList<HashMap<String, String>>();
+        ListView lv = getListView();
+
+        String result="";
+        try {
+            result = new getData().execute("http://emoncms.org/feed/list.json?apikey=8133697b1b562f52689bd680b330cb4d").get();
+        } catch (Exception e) {
+            Log.i("EmonLog", "Error in getData");
+        }
+        Log.i("EmonLog", "Result from getData: " + result);
+
+        Log.i ("Emonlog","Parse result from server");
+        if (result != null) {
+            try {
+                JSONArray jsonArr = new JSONArray(result);
+
+                // looping through All items
+                for (int i = 0; i < jsonArr.length(); i++) {
+
+                    JSONObject c = jsonArr.getJSONObject(i);
+                    String id = c.getString(TAG_ID);
+                    String name = c.getString(TAG_NAME);
+                    String value = c.getString(TAG_VALUE);
+                    String time = c.getString(TAG_TIME);
+
+                    HashMap<String, String> item = new HashMap<String, String>();
+
+                    // Convert timestamp
+                    long tlong = Long.parseLong(time);
+                    String realtime = getDate(tlong);
+                    Log.i("Timelong: ",realtime);
+                    datatime = realtime;
+                    item.put(TAG_NAME, name + ": " + value);
+                    dataList.add(item);
+
+                    // Update header
+                    header.setText("Emoncms data, retrived @ " + datatime);
+
+                    ListAdapter adapter = new SimpleAdapter(
+                            MainActivity.this, dataList,
+                            R.layout.list_item, new String[]{TAG_NAME}, new int[]{R.id.name});
+                    setListAdapter(adapter);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-
+    private String getDate(long time) {
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(time * 1000);
+        String date = DateFormat.format("yyyy-MM-dd HH:mm", cal).toString();
+        return date;
+    }
 }
---
+++ app/src/main/java/nu/paheco/patrik/emondatalight/getData.java
@@ -0,0 +1,36 @@
+package nu.paheco.patrik.emondatalight;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+class getData extends AsyncTask<String, Void, String> {
+
+    // URL to get Emoncms JSON data
+    private static String url = "http://emoncms.org/feed/list.json?apikey=8133697b1b562f52689bd680b330cb4d";
+
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_TIME = "time";
+    private static final String TAG_VALUE = "value";
+
+    @Override
+    protected String doInBackground(String... params) {
+        String datatime="";
+
+        String urlstring = params[0];
+        Log.i("EmonLog", "HTTP Connecting: " + urlstring);
+
+        //URL url = new URL(urlstring);
+
+        // Creating service handler class instance
+        ServiceHandler sh = new ServiceHandler();
+        // Making a request to url and getting response
+        String jsonStr = sh.makeServiceCall(urlstring, ServiceHandler.GET);
+        Log.d("Response: ", "> " + jsonStr);
+
+        Log.w("In getData","Ok got data");
+        return jsonStr;
+
+    }
+
+}
